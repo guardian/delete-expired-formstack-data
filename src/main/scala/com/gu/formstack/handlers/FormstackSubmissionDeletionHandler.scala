b44d2d020b562918e7825606f45a48b863cbf552
Mullefa <guy.dawson@guardian.co.uk>
2021-01-22 12:47:32 +0000

    dummy execution of lambdas whilst deployment is setup

--- src/main/scala/com/gu/formstack/handlers/FormstackSubmissionDeletionHandler.scala
+++ src/main/scala/com/gu/formstack/handlers/FormstackSubmissionDeletionHandler.scala
@@ -1,25 +1,23 @@
 package com.gu.formstack.handlers
 
-import cats.implicits._
 import com.amazonaws.services.lambda.runtime.{ Context, RequestStreamHandler }
 import com.gu.formstack.Logging
-import com.gu.formstack.config.Config
-import com.gu.formstack.services.FormstackSubmissionDeleter
 
 import java.io.{ InputStream, OutputStream }
 
 object FormstackSubmissionDeletionHandler extends RequestStreamHandler with Logging {
 
   override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
-    (for {
-      config <- Config.fromEnvironmentVariables()
-      _ = logger.info(show"executing submission deletions with config ${config.hideSecrets}")
-      _ <- Logging.configureLogging(config, context)
-      submissionDeleter = FormstackSubmissionDeleter.fromConfig(config)
-      _ <- submissionDeleter.deleteExpiredSubmissions()
-    } yield ()).fold(
-      err => logger.error("error deleting expired submissions", err),
-      _ => ()
-    )
+    println("executing lambda")
+//    (for {
+//      config <- Config.fromEnvironmentVariables()
+//      _ = logger.info(show"executing submission deletions with config ${config.hideSecrets}")
+//      _ <- Logging.configureLogging(config, context)
+//      submissionDeleter = FormstackSubmissionDeleter.fromConfig(config)
+//      _ <- submissionDeleter.deleteExpiredSubmissions()
+//    } yield ()).fold(
+//      err => logger.error("error deleting expired submissions", err),
+//      _ => ()
+//    )
   }
 }
