23c40f8b8f7ea1bd12d30de38ae72d0775bdc9d6
Mullefa <guy.dawson@guardian.co.uk>
2021-01-22 22:33:44 +0000

    add alarms

--- src/main/scala/com/gu/formstack/handlers/FormstackFormDeletionHandler.scala
+++ src/main/scala/com/gu/formstack/handlers/FormstackFormDeletionHandler.scala
@@ -1,23 +1,32 @@
 package com.gu.formstack.handlers
 
+import cats.implicits._
 import com.amazonaws.services.lambda.runtime.{ Context, RequestStreamHandler }
 import com.gu.formstack.Logging
+import com.gu.formstack.config.Config
+import com.gu.formstack.services.FormstackFormDeleter
 
 import java.io.{ InputStream, OutputStream }
 
 object FormstackFormDeletionHandler extends RequestStreamHandler with Logging {
 
   override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
-    println("executing lambda")
-//    (for {
-//      config <- Config.fromEnvironmentVariables()
-//      _ = logger.info(show"executing form deletions with config ${config.hideSecrets}")
-//      _ <- Logging.configureLogging(config, context)
-//      formDeleter = FormstackFormDeleter.fromConfig(config)
-//      _ <- formDeleter.deleteExpiredForms()
-//    } yield ()).fold(
-//      err => logger.error("error in form deletion process", err),
-//      _ => ()
-//    )
+    (for {
+      config <- Config.fromEnvironmentVariables()
+      _ <- Logging.configureLogging(config, context)
+      _ = logger.info(show"executing form deletions with config ${config.hideSecrets}")
+      formDeleter = FormstackFormDeleter.fromConfig(config)
+      _ <- formDeleter.deleteExpiredForms()
+    } yield ()).fold(
+      err => {
+        logger.error("error deleting expired forms", err)
+        // It's important to throw this exception, so that the invocation will be recorded as an error.
+        // Invocation retries and alarms being triggered are determined by invocation errors;.
+        // See https://docs.aws.amazon.com/lambda/latest/dg/invocation-async.html and template.yaml (respectively)
+        // for more details.
+        throw err
+      },
+      _ => logger.info("expired forms successfully deleted")
+    )
   }
 }
--- src/main/scala/com/gu/formstack/handlers/FormstackSubmissionDeletionHandler.scala
+++ src/main/scala/com/gu/formstack/handlers/FormstackSubmissionDeletionHandler.scala
@@ -1,23 +1,32 @@
 package com.gu.formstack.handlers
 
+import cats.implicits._
 import com.amazonaws.services.lambda.runtime.{ Context, RequestStreamHandler }
 import com.gu.formstack.Logging
+import com.gu.formstack.config.Config
+import com.gu.formstack.services.FormstackSubmissionDeleter
 
 import java.io.{ InputStream, OutputStream }
 
 object FormstackSubmissionDeletionHandler extends RequestStreamHandler with Logging {
 
   override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
-    println("executing lambda")
-//    (for {
-//      config <- Config.fromEnvironmentVariables()
-//      _ = logger.info(show"executing submission deletions with config ${config.hideSecrets}")
-//      _ <- Logging.configureLogging(config, context)
-//      submissionDeleter = FormstackSubmissionDeleter.fromConfig(config)
-//      _ <- submissionDeleter.deleteExpiredSubmissions()
-//    } yield ()).fold(
-//      err => logger.error("error deleting expired submissions", err),
-//      _ => ()
-//    )
+    (for {
+      config <- Config.fromEnvironmentVariables()
+      _ <- Logging.configureLogging(config, context)
+      _ = logger.info(show"executing submission deletions with config ${config.hideSecrets}")
+      submissionDeleter = FormstackSubmissionDeleter.fromConfig(config)
+      _ <- submissionDeleter.deleteExpiredSubmissions()
+    } yield ()).fold(
+      err => {
+        logger.error("error deleting expired submissions", err)
+        // It's important to throw this exception, so that the invocation will be recorded as an error.
+        // Invocation retries and alarms being triggered are determined by invocation errors;.
+        // See https://docs.aws.amazon.com/lambda/latest/dg/invocation-async.html and template.yaml (respectively)
+        // for more details.
+        throw err
+      },
+      _ => logger.info("expired submissions successfully deleted")
+    )
   }
 }
