278fafed7402287b373640654fea56ad8ce7aa6d
Mullefa <guy.dawson@guardian.co.uk>
2021-01-27 21:27:39 +0000

    only one stack

--- src/main/scala/com/gu/formstack/handlers/FormstackFormDeletionHandler.scala
+++ src/main/scala/com/gu/formstack/handlers/FormstackFormDeletionHandler.scala
@@ -3,10 +3,12 @@ package com.gu.formstack.handlers
 import com.amazonaws.services.lambda.runtime.{ Context, RequestStreamHandler }
 
 import java.io.{ InputStream, OutputStream }
+import scala.io.Source
 
 object FormstackFormDeletionHandler extends RequestStreamHandler {
 
   override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
-    println("executing updated lambda")
+    val input = Source.fromInputStream(input).mkString
+    println(s"executing updated lambda: $input")
   }
 }
